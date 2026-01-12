9e442a3dc3ac530111b780d7b407214c6861f186
Monica Santamaria <santamariamonica31@gmail.com>
2026-01-12 01:32:18 -0500

    Añadir persistencia con JSON y funcionalidad de eliminar tareas

--- app.py
+++ app.py
@@ -1,24 +1,53 @@
 from flask import Flask, request, render_template, redirect, url_for
+import json
 
 app = Flask(__name__)
 
 tareas = []
 siguiente_id = 1
 
+def guardar_datos():
+    with open('tareas.json', 'w') as f:
+        json.dump({'siguiente_id': siguiente_id, 'tareas': tareas}, f)
+
+def cargar_datos():
+    global siguiente_id, tareas
+    try:
+        with open('tareas.json', 'r') as f:
+            data = json.load(f)
+            tareas = data['tareas']
+            siguiente_id = data['siguiente_id']
+    except FileNotFoundError:
+        pass
+
+cargar_datos()
+
 def agregar_tarea(texto):
     global siguiente_id
     tareas.append({'id': siguiente_id, 'texto': texto, 'hecho': False})
     siguiente_id += 1
+    guardar_datos()
 
 def completar_tarea(id):
     for tarea in tareas:
         if tarea['id'] == id:
             tarea['hecho'] = True
             break
+    guardar_datos()
+
+def eliminar_tarea(id):
+    global tareas
+    tareas = [t for t in tareas if t['id'] != id]
+    guardar_datos()
 
 @app.route('/completar/<int:id>')
 def completar(id):
     completar_tarea(id)
+    return redirect('/')
+
+@app.route('/eliminar/<int:id>')
+def eliminar(id):
+    eliminar_tarea(id)
     return redirect('/')
 
 @app.route('/')
